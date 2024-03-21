0c7ea25b397a180b522c61db543445c4c80554d6
lmw7060 <dlaalsdn7@naver.com>
2024-03-21 11:26:48 +0900

    5

---
+++ class01/homework/lmw7060/hw5_Percaptron_ANN/dtraining.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+
+model = tf.keras.models.load_model('model.h5')
+
+mnist = tf.keras.datasets.mnist
+(image_train, label_train),(image_test, label_test) = \
+mnist.load_data()
+
+NUM = 5
+predict = model.predict(image_test[0:NUM])
+print(predict)
+
+print('* prediction',np.argmax(predict, axis=1))
+
+plt.figure(figsize=(15, 15))
+for idx in range(NUM):
+    sp = plt.subplot(1, 5, idx+1)
+    plt.imshow(image_test[idx])
+plt.show()
